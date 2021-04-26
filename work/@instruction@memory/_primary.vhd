c3fbc4d89d4ca480f6ad653a1a0a95b4a5327938
Amin Asadi <aminasadi329@gmail.com>
2021-04-26 14:33:59 +0430

    clean repository

--- work/@instruction@memory/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity InstructionMemory is
-    port(
-        address         : in     vl_logic_vector(31 downto 0);
-        instruction     : in     vl_logic_vector(31 downto 0)
-    );
-end InstructionMemory;
--- work/@m@u@x2to1@t@b/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity MUX2to1TB is
-end MUX2to1TB;
