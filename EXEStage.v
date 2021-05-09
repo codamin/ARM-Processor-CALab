7bae7de54202f6cbce17e3c6eada66b1ee02668d
hoenza <h.entezari78@ut.ac.ir>
2021-05-09 15:24:06 +0430

    to carry

--- EXEStage.v
+++ EXEStage.v
@@ -11,7 +11,6 @@ module EXEStage(rst, clk, carry, EXE_CMD, MEM_R_EN, MEM_W_EN, PC, Val_Rn, Val_Rm
   input[3:0] SR;
   output[31:0] ALU_result, Br_addr;
   output[3:0] status;
-  output[3:0]
   
   wire LoS;
   wire[31:0] val2;
@@ -21,5 +20,5 @@ module EXEStage(rst, clk, carry, EXE_CMD, MEM_R_EN, MEM_W_EN, PC, Val_Rn, Val_Rm
   ALU aLU(Val_Rn, val2, carry, EXE_CMD, ALU_result, ALU_status);
   Value2Generator value2Generator(LoS, Val_Rm, imm, Shift_operand, val2);
   assign Br_addr = PC + Signed_imm_24;
-  StatusRegister statusRegister(rst, clk, S?, ALU_status, status);
+  StatusRegister statusRegister(rst, clk, S, ALU_status, status);
 endmodule
--- MainTB.v
+++ MainTB.v
@@ -1,14 +1,12 @@
 `timescale 1ns/1ns
-module MainTB();
-  reg clk=0, rst=0;
 
+module MainTB();
+  reg rst=0, clk=0;
   Main UUT(rst, clk);
-  
-  always #10 clk = ~clk;
+  always #10 clk=~clk;
   initial begin
-    #5 rst = 1;
-    #5 rst = 0;
-    #2000 $stop;
+    rst = 1;
+    #25 rst = 0;
+    #1000 $stop;
   end
-  
 endmodule
